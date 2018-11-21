73b06402c0975b1c46302a0252d21ef4e65ba713
sandrine <poupardsandrine@gmail.com>
2018-11-21 13:03:28 +0100

    firsname and atop finished problem :checkbox box not visible

--- app/src/main/java/fr/wcs/quetejavaui/MainActivity.java
+++ app/src/main/java/fr/wcs/quetejavaui/MainActivity.java
@@ -2,16 +2,26 @@ package fr.wcs.quetejavaui;
 
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue;
+import android.text.InputType;
+import android.util.LayoutDirection;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +35,11 @@ public class MainActivity extends AppCompatActivity {
         TextView welcomeTV = new TextView(context);
         // Create welcome TextView Layout
         LinearLayout.LayoutParams welcomeTVLayout = new LinearLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT, // Width of TextView
-                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+                LinearLayout.LayoutParams.MATCH_PARENT, // Width of TextView
+                LinearLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
         //change welcome TextView layaout properties
         welcomeTVLayout.setMargins(0,10,0,10);
-        // Apply the layout parameters to TextView widget
+        // Apply the layout parameters to welcome TextView widget
         welcomeTV.setLayoutParams(welcomeTVLayout);
         //change welcome TextView properties
         welcomeTV.setBackgroundColor(Color.parseColor("#ff4081"));
@@ -37,38 +47,67 @@ public class MainActivity extends AppCompatActivity {
         welcomeTV.setTextSize(20);
         welcomeTV.setPadding(10 ,5,10,5);
         welcomeTV.setText(R.string.welcome_text);
-
+        // add welcome TextView to mainLayaout
         layout.addView(welcomeTV);
 
 
+        // Create wantBeWilderCB CheckBox programmatically.
+        CheckBox wantBeWilderCB = new CheckBox(context);
+        // Create wantBeWilderCB CheckBox Layout
+        LinearLayout.LayoutParams wantBeWilderCBLayout = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                LinearLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+        // Apply the layout parameters to wantBeWilderCB CheckBox widget
+        wantBeWilderCB.setLayoutParams(welcomeTVLayout);
+        //change wantBeWilderCB CheckBox properties
+        wantBeWilderCB.setGravity(Gravity.CENTER_HORIZONTAL);
+        wantBeWilderCB.setText(R.string.want_to_be_wilder_text);
+        //needed for my phone
+        wantBeWilderCB.setTextColor(Color.parseColor("#000000"));
+        // add wantBeWilderCB CheckBox to mainLayaout
+        layout.addView(wantBeWilderCB);
+
+        //create  textsEditsButtonLL linearLayout
+        LinearLayout textsEditsButtonLL = new LinearLayout(context);
+        // Create textsEditsButtonLL linearLayout LinearLayout parameters
+        LinearLayout.LayoutParams textsEditsButtonLLP = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, // Width of TextView
+                LinearLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+        // Apply the layout parameters to textsEditsButtonLL linearLayout
+       textsEditsButtonLL.setLayoutParams(textsEditsButtonLLP);
+       textsEditsButtonLL.setOrientation(LinearLayout.HORIZONTAL);
+       textsEditsButtonLL.setWeightSum(2);
+       textsEditsButtonLL.setBackgroundColor(Color.parseColor("#374081"));
+        // add textsEditsButtonLL linearLayout to mainLayaout
+        layout.addView(textsEditsButtonLL);
+
+        // Create firstnameET EditText programmatically.
+        EditText firstnameET = new EditText(context);
+        // Create firstnameET EditText Layout
+        LinearLayout.LayoutParams firstnameETLayout = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, // Width of TextView
+                LinearLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+        // Apply the layout parameters to firstnameET EditText widget
+        firstnameET.setLayoutParams(welcomeTVLayout);
+        //change firstnameET EditText properties
+        firstnameET.setEms(10);
+        firstnameET.setGravity(1);
+        firstnameET.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        firstnameET.setHint(R.string.firstname_text);
+        //needed for my phone
+        firstnameET.setHintTextColor(Color.GRAY);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            firstnameET.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+        }
+        // add firstnameET EditText to mainLayaout
+        layout.addView(firstnameET);
+
+
 /*
-
-desiredSp = getResources().getDimension(R.dimen.desired_sp);
-density = getResources().getDisplayMetrics().density;
-textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, desiredSp / density);
+        <LinearLayout textsEditsButtonLL linearLayout
 
 
-        <CheckBox
-            android:id="@+id/checkbox"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_gravity="center_horizontal"
-            android:text="Yes, I want to be a Wilder" />
 
-        <LinearLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:orientation="horizontal"
-            android:weightSum="2" >
-
-            <EditText
-                android:id="@+id/firstname"
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:ems="10"
-                android:layout_weight="1"
-                android:inputType="textPersonName"
-                android:hint="Firstname" />
 
             <Space
                 android:layout_width="match_parent"
